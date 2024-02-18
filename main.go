ffe7214482eb3d507787b8612e2c05aa3a0dd0f8
vHideyukiHoukawa <hideyuki.houkawa@veriserve.co.jp>
2024-02-18 20:18:51 +0900

    :sparkles: implement total access count limit; show additional usage help

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 // Constants
@@ -20,7 +22,10 @@ const (
 // Variables
 var (
 	port               int                     // Port number to listen on
-	trueKey            string                  // Real OpenAI API key
+	accessCountLimit   int64                   // Total access count limit
+	accessCounter      int64                   // Counter to track the total access count
+	mu                 sync.Mutex              // Mutex to synchronize accessCounter updates
+	realKey            string                  // Real OpenAI API key
 	virtualKeyFilePath string                  // File path for virtual OpenAI keys
 	virtualKeys        = make(map[string]bool) // Map to store virtual OpenAI keys
 )
@@ -28,16 +33,36 @@ var (
 // init function to initialize command-line flags
 func init() {
 	// Define command-line flags
-	flag.IntVar(&port, "port", 48080, "Port number to listen on")
+	flag.IntVar(&port, "port", 48080, "Port number to listen on.")
+	flag.Int64Var(&accessCountLimit, "access-count-limit", -1, "Total access count limit. Use -1 for no limit.")
 	flag.StringVar(&virtualKeyFilePath, "vkeys", "virtual_api_keys.txt", "File path for virtual OpenAI keys")
+
+	additionalHelp1 := `
+This program offers reverse proxy functionality to the OpenAI API server with additional features, including:
+  1. Abuse protection through a total access count limit.
+  2. Enhanced security by using virtual API keys instead of exposing the real API key.
+`
+	additionalHelp2 := fmt.Sprintf(`
+Note:
+  * Set your real OpenAI API key as the environment variable: %s.
+  * Configure your app's OpenAI API access to use http://<ip-or-hostname-of-this-machine>:<port>/v1.
+    Ensure the path includes "/v1".
+`, EVN_OPENAI_API_KEY)
+
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, additionalHelp1)
+		fmt.Fprintf(os.Stderr, "\nUsage: %s [options]\n\n", os.Args[0])
+		flag.PrintDefaults()
+		fmt.Fprint(os.Stderr, additionalHelp2)
+	}
 }
 
 // config function to get the real OpenAI API key from the environment variables and read virtual API keys from a file
 func config() {
 	// Get the real OpenAI API key from the environment variables
-	trueKey = os.Getenv(EVN_OPENAI_API_KEY)
-	if trueKey == "" {
-		log.Fatal(errors.New(EVN_OPENAI_API_KEY + " environment variable is not defined"))
+	realKey = os.Getenv(EVN_OPENAI_API_KEY)
+	if realKey == "" {
+		log.Fatal(errors.New(EVN_OPENAI_API_KEY + " environment variable is not defined. Please set a real OpenAI key for this"))
 	}
 
 	// Log the loading of virtual API keys from a file
@@ -46,7 +71,7 @@ func config() {
 	// Read virtual API keys from the specified file
 	content, err := os.ReadFile(virtualKeyFilePath)
 	if err != nil {
-		log.Fatal("Error reading file:", err)
+		log.Fatalf("****** Error: %s\nPlease provide virtual API keys in the file: %s, with each key on a separate line.", err, virtualKeyFilePath)
 	}
 
 	// Populate the virtualKeys map with the read keys
@@ -60,8 +85,23 @@ func config() {
 
 // ReverseProxyHandler handles incoming HTTP requests and forwards them to the OpenAI API with proper authentication
 func ReverseProxyHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Increment the access counter while protecting it with a mutex
+	mu.Lock()
+	accessCounter++
+	count := accessCounter
+	mu.Unlock()
+
 	// Log information about the incoming request
-	log.Printf("*** request from %s with auth: %s\n", r.RemoteAddr, r.Header.Get("Authorization"))
+	// log.Printf("*** request No.%d from %s with auth: %s\n", count, r.RemoteAddr, r.Header.Get("Authorization"))
+	log.Printf("*** request No.%d from %s\n", count, r.RemoteAddr)
+
+	// Check total access limit and return error if exceeded
+	if accessCountLimit != -1 && count > accessCountLimit {
+		log.Printf("****** Warning: Total access limit of %d exceeded", accessCountLimit)
+		http.Error(w, "Total access count limit exceeded.", http.StatusUnprocessableEntity)
+		return
+	}
 
 	// Set the target OpenAI API and initialize the key variable
 	target := HOST_OPENAI_API
@@ -75,7 +115,7 @@ func ReverseProxyHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Check if the key is a virtual key, if not, use the real key
 	if _, exists := virtualKeys[key]; exists {
-		key = trueKey
+		key = realKey
 	} else {
 		log.Printf("****** Warning: No virtual key found")
 	}
@@ -93,7 +133,7 @@ func ReverseProxyHandler(w http.ResponseWriter, r *http.Request) {
 	proxy.ServeHTTP(w, r)
 
 	// Log information about the response headers
-	log.Printf("*** response with header: %s\n", w.Header())
+	// log.Printf("*** response with header: %s\n", w.Header())
 }
 
 // main function to start the HTTP server
@@ -101,7 +141,7 @@ func main() {
 	// Parse command-line flags
 	flag.Parse()
 
-	// Configure varoius keys
+	// Setup keys configuration
 	config()
 
 	// Log the start of the server
